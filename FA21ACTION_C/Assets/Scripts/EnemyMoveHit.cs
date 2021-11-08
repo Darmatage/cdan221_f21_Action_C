aeeb42efb49bd79990b0e5b7584a52338532de3a
JasonWiser <JasonWiserArt@gmail.com>
2021-11-08 18:35:11 -0500

    Added placeholder Faith scene and prefabs: player (wip), enemyA, GameHandler

---
+++ FA21ACTION_C/Assets/Scripts/EnemyMoveHit.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class EnemyMoveHit : MonoBehaviour {
+
+       public Animator anim;
+       public float speed = 4f;
+       private Transform target;
+       public int damage = 10;
+
+       public int EnemyLives = 3;
+       private Renderer rend;
+       private GameHandler gameHandler;
+
+       public float attackRange = 10;
+
+       void Start () {
+              rend = GetComponentInChildren<Renderer> ();
+
+              if (GameObject.FindGameObjectWithTag ("Player") != null) {
+                     target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+              }
+
+              if (GameObject.FindWithTag ("GameHandler") != null) {
+                  gameHandler = GameObject.FindWithTag ("GameHandler").GetComponent<GameHandler> ();
+              }
+       }
+
+       void Update () {
+              float DistToPlayer = Vector3.Distance(transform.position, target.position);
+
+              if ((target != null) && (DistToPlayer <= attackRange)){
+                     transform.position = Vector2.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+              }
+       }
+
+       public void OnCollisionEnter2D(Collision2D collision){
+              if (collision.gameObject.tag == "Player") {
+                     anim.SetBool("Attack", true);
+                     gameHandler.playerGetHit(damage);
+                     //rend.material.color = new Color(2.4f, 0.9f, 0.9f, 0.5f);
+                     //StartCoroutine(HitEnemy());
+              }
+       }
+
+       public void OnCollisionExit2D(Collision2D collision){
+              if (collision.gameObject.tag == "Player") {
+                     anim.SetBool("Attack", false);
+              }
+       }
+
+       IEnumerator HitEnemy(){
+              yield return new WaitForSeconds(0.5f);
+              rend.material.color = Color.white;
+       }
+
+       //DISPLAY the range of enemy's attack when selected in the Editor
+       void OnDrawGizmosSelected(){
+              Gizmos.DrawWireSphere(transform.position, attackRange);
+       }
+}
---
+++ FA21ACTION_C/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour {
+
+      private GameObject player;
+      public static int playerHealth;
+      public int StartPlayerHealth = 100;
+      public GameObject healthText;
+
+      public static int gotTokens = 0;
+      public GameObject tokensText;
+
+      public bool isDefending = false;
+
+      public static bool stairCaseUnlocked = false;
+      //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
+
+      void Start(){
+            player = GameObject.FindWithTag("Player");
+            playerHealth = StartPlayerHealth;
+            updateStatsDisplay();       
+      }
+
+      public void playerGetTokens(int newTokens){
+            gotTokens += newTokens;
+            updateStatsDisplay();
+      }
+
+      public void playerGetHit(int damage){
+           if (isDefending == false){
+                  playerHealth -= damage;
+                  updateStatsDisplay();
+                  player.GetComponent<PlayerHurt>().playerHit();
+            }
+
+           if (playerHealth >= StartPlayerHealth){
+                  playerHealth = StartPlayerHealth;
+            }
+
+           if (playerHealth <= 0){
+                  playerHealth = 0;
+                  playerDies();
+            }
+      }
+
+      public void updateStatsDisplay(){
+            Text healthTextTemp = healthText.GetComponent<Text>();
+            healthTextTemp.text = "HEALTH: " + playerHealth;
+
+            Text tokensTextTemp = tokensText.GetComponent<Text>();
+            tokensTextTemp.text = "GOLD: " + gotTokens;
+      }
+
+      public void playerDies(){
+            player.GetComponent<PlayerHurt>().playerDead();
+            StartCoroutine(DeathPause());
+      }
+
+      IEnumerator DeathPause(){
+            //player.GetComponent<PlayerMove>().isAlive = false;
+            //player.GetComponent<PlayerJump>().isAlive = false;
+            yield return new WaitForSeconds(1.0f);
+            SceneManager.LoadScene("EndLose");
+      }
+
+      public void StartGame() {
+            SceneManager.LoadScene("Level1");
+      }
+
+      public void RestartGame() {
+            SceneManager.LoadScene("MainMenu");
+            playerHealth = StartPlayerHealth;
+      }
+
+      public void QuitGame() {
+                #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                Application.Quit();
+                #endif
+      }
+
+      public void Credits() {
+            SceneManager.LoadScene("Credits");
+      }
+}
