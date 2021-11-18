a7f1ea2d21f6b6dabcb38d73f4212b7b03314f87
sgarzy <91702039+sgarzy@users.noreply.github.com>
2021-11-18 18:32:23 -0500

    audio thing

---
+++ FA21ACTION_C/Assets/Scripts/GarzonAudioTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarzonAudioTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+public AudioSource whack ; 
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      if (Input .GetKeyDown ("5") ) {whack.Play();}
+
+
+    }
+}
