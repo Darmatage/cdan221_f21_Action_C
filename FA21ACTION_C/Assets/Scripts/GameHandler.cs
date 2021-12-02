f51fd58858e5615c39cb095966261f84519520d5
JasonWiser <JasonWiserArt@gmail.com>
2021-12-02 14:53:35 -0500

    Pause menu works! Also, audio manager is ready for music

--- FA21ACTION_C/Assets/Scripts/GameHandler.cs
+++ FA21ACTION_C/Assets/Scripts/GameHandler.cs
@@ -3,27 +3,75 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
 
 public class GameHandler : MonoBehaviour {
 
-      private GameObject player;
-      public static int playerHealth;
-      public int StartPlayerHealth = 100;
-      public GameObject healthText;
+	private GameObject player;
+    public static int playerHealth;
+    public int StartPlayerHealth = 100;
+    public GameObject healthText;
 
-      public static int gotTokens = 0;
-      public GameObject tokensText;
+    public static int gotTokens = 0;
+    public GameObject tokensText;
 
-      public bool isDefending = false;
+    public bool isDefending = false;
 
-      public static bool stairCaseUnlocked = false;
+    public static bool stairCaseUnlocked = false;
       //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
 
-      void Start(){
-            player = GameObject.FindWithTag("Player");
-            playerHealth = StartPlayerHealth;
-            updateStatsDisplay();       
-      }
+        public static bool GameisPaused = false;
+        public GameObject pauseMenuUI;
+        public AudioMixer mixer;
+        public static float volumeLevel = 1.0f;
+        private Slider sliderVolumeCtrl;
+
+
+        void Awake (){
+                SetLevel (volumeLevel);
+                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+                if (sliderTemp != null){
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+                        sliderVolumeCtrl.value = volumeLevel;
+                }
+        }
+
+	void Start(){
+		pauseMenuUI.SetActive(false);
+		player = GameObject.FindWithTag("Player");
+		playerHealth = StartPlayerHealth;
+		updateStatsDisplay();       
+	}
+
+	void Update (){
+		if (Input.GetKeyDown(KeyCode.Escape)){
+			if (GameisPaused){
+				Resume();
+			}
+			else{
+				Pause();
+			}
+		}
+	}
+
+	void Pause(){
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameisPaused = true;
+	}
+
+        public void Resume(){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                GameisPaused = false;
+        }
+
+        public void SetLevel (float sliderValue){
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                volumeLevel = sliderValue;
+        } 
+
 
       public void playerGetTokens(int newTokens){
             gotTokens += newTokens;
@@ -71,10 +119,11 @@ public class GameHandler : MonoBehaviour {
             SceneManager.LoadScene("LevelScene_0");
       }
 
-      public void RestartGame() {
-            SceneManager.LoadScene("MainMenu");
-            playerHealth = StartPlayerHealth;
-      }
+	public void RestartGame() {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("MainMenu");
+		playerHealth = StartPlayerHealth;
+	}
 
       public void QuitGame() {
                 #if UNITY_EDITOR
