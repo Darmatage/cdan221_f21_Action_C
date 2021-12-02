9f9f34245e932511b6e06bab5c30862935c229b8
ErinDavila <eddavila@massart.edu>
2021-12-02 11:51:24 -0500

    added dorm rooms (empty save for enemies in a few), changed basic obstacles along levels 1-4

--- FA21ACTION_C/Assets/Scripts/DoorOpen_Enemy1.cs
+++ FA21ACTION_C/Assets/Scripts/DoorOpen_Enemy1.cs
@@ -1,13 +1,12 @@
-
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DoorOpen_MainLevel : MonoBehaviour
+public class DoorOpen_Enemy1 : MonoBehaviour
 {
 
-    public string NextLevel = "LevelScene_1";
+    public string NextLevel = "DormRoomScene_Enemy";
 
     public void OnTriggerEnter2D(Collider2D other)
     {
