0d43200948ac1dd4fe5350b63c92eb928c33bd6a
JasonWiser <JasonWiserArt@gmail.com>
2021-12-10 23:52:33 -0500

    updated enemy prefabs and obstacle spritesheet (placeholder)

---
+++ FA21ACTION_C/Assets/Scripts/EnemyMeleeDamage.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class EnemyMeleeDamage : MonoBehaviour {
+
+	public Animator anim;
+	public GameObject healthLoot;
+	public int maxHealth = 100;
+	public int currentHealth;
+
+	void Start(){
+		currentHealth = maxHealth;
+		anim = gameObject.GetComponent<Animator>();
+	}
+
+	public void TakeDamage(int damage){
+		currentHealth -= damage;
+		//anim.SetTrigger ("Hurt");
+		if (currentHealth <= 0){
+			Die();
+		}
+	}
+
+	void Die(){
+		Instantiate (healthLoot, transform.position, Quaternion.identity);
+		//anim.SetBool ("isDead", true);
+		GetComponent<Collider2D>().enabled = false;
+		this.enabled = false;
+		StartCoroutine(Death());
+	}
+
+	IEnumerator Death(){
+		yield return new WaitForSeconds(0.5f);
+		Debug.Log("You Killed a baddie. You deserve loot!");
+		Destroy(gameObject);
+	}
+
+} 
