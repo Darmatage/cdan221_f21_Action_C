77db90e0469b188fd6b4a3367b3c2cb6517dbfa7
JasonWiser <JasonWiserArt@gmail.com>
2021-12-17 13:37:44 -0500

    door functionality for keycards progressed

--- FA21ACTION_C/Assets/Scripts/Door.cs
+++ FA21ACTION_C/Assets/Scripts/Door.cs
@@ -9,7 +9,13 @@ public class Door : MonoBehaviour
 
     public string NextLevel = "DormRoomScene";
 	public GameObject pressE;
-	public bool canOpen = false;
+	public GameObject doorLocked;
+	private bool canOpen = false;
+	public bool isLocked = false;
+	
+	public string LockedKey;
+	
+	
 
 	void Start(){
 		pressE.SetActive(false);
@@ -24,16 +30,38 @@ public class Door : MonoBehaviour
 
     public void OnTriggerStay2D(Collider2D other){
         if (other.gameObject.tag == "Player"){
-			pressE.SetActive(true);
-            canOpen = true;
+				
+			if (isLocked== false){
+				pressE.SetActive(true);
+				canOpen = true;
+			}
+			else if (isLocked== true){
+				if (playerHasKey()){
+					pressE.SetActive(true);
+					canOpen = true;
+				}
+				else {doorLocked.SetActive(true);}
+			}
+			
+			
         }
     }
 	
 	public void OnTriggerExit2D(Collider2D other){
         if (other.gameObject.tag == "Player"){
 			pressE.SetActive(false);
+			doorLocked.SetActive(true);
             canOpen = false;
         }
     }
+	
+	public bool playerHasKey(){
+		return true; // temporary true
+		
+		// if (Inventory.checkkeys has LockedKey)
+		// {return true;}
+		// else{ return false;}
+	}
+	
 
 }
